f997d717d9db316b95a8dadfb6bd6d38d196315d
VALLAUD-Ulrich <ulrichvallaud@hotmail.fr>
2022-10-31 11:22:32 +0100

    1.0 simple serveur ok

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-const router = require('./app/routers/routers/router');
+const router = require('./app/routers');
 
-app.use('/', router);
+app.use(router);
 
 
 app.listen(port, () => {
